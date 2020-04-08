b42e66ad820e4b01842b526c88c632dc90ccb00c
sjadriana <adriana.sj@live.com>
2020-04-08 15:56:18 -0300

    codigos cifrar e decifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,28 @@
-const cipher = {
-  // ...
-};
+let cipher = {
+  
+  encode: function (senha, mensagem) { 
+    let resultado= "";
+    for (let i = 0; i < mensagem.length; i++)  {
+      let cifra=((mensagem[i].charCodeAt() - 65 + Number(senha)) % 26) + 65;
+      resultado = resultado + String.fromCharCode(cifra);
+    }  
+    
 
+    return resultado;
+  },  
+  
+  decode: function (senha, mensagem) {
+   
+    let resultado= "";
+    for (let i = 0; i < mensagem.length; i++) {
+      let cifra=((mensagem[i].charCodeAt() + 65 - Number(senha)) % 26) + 65;
+      resultado = resultado + String.fromCharCode(cifra);
+    }  
+    
+    return resultado;
+  },  
+};
 export default cipher;
+  
+ 
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 import cipher from './cipher.js';
+//alert('Escreva sua mensagem e escolha a opção desejada');
+    
+console.log(cipher);
+
+var botaoC = document.getElementById("btn1")
+botaoC.addEventListener("click", function(event){
+
+  event.preventDefault();
+
+  let entrada = document.getElementById("origem").value.toUpperCase().trim();
+  let senha =document.getElementById("offset").value;
+
+  let saida= cipher.encode(senha, entrada);
+  
+  document.getElementById("destino").innerHTML= saida;
+
+});
+
+let botaoD = document.getElementById("btn2")
+botaoD.addEventListener("click", function(event){
+
+  event.preventDefault();
+
+  let entrada = document.getElementById("origem").value.toUpperCase().trim();
+  let senha =document.getElementById("offset").value;
+
+  let saida= cipher.decode(senha, entrada);
+  let destino = document.getElementById("destino")
+  destino.innerHTML= saida;
+
+});
+/*function cifrar (event) {
+    event.preventDefault();
+    
+    let tEntrada = document.getElementById("origem").value;
+    let entrada = tEntrada.toUpperCase();    
+    let senha = document.getElementById("offset").value;
+    let destino = document.getElementById("destino");
+    let saida = cipher.encode(senha, entrada);
+    
+   
+   destino.innerHTML = saida
+}
+document.getElementById("btn1").addEventListener("click", cifrar);
+//
+//function decifrar(){}*/
+
 
 console.log(cipher);
